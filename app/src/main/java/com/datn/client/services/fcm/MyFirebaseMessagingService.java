d43fcb6d3b4393ef99409ef8b232634508738779
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-02-29 17:58:55 +0700

    add notification

---
+++ app/src/main/java/com/datn/client/services/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,72 @@
+package com.datn.client.services.fcm;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.datn.client.R;
+import com.datn.client.ui.LoginActivity;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+        if (message.getData().size() > 0) {
+            String title = message.getData().get("title");
+            String body = message.getData().get("body");
+            Log.w("onMessageReceived", title + "-" + body);
+            sendNotification(title, body);
+        }
+    }
+
+    private void sendNotification(String title, String messageBody) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "Stech", "Stech",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, "Stech")
+                        .setSmallIcon(R.drawable.logo)
+                        .setCustomContentView(createNotificationView(title, messageBody))
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    @NonNull
+    @SuppressLint("RemoteViewLayout")
+    private RemoteViews createNotificationView(String title, String content) {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.layout_notification);
+        remoteViews.setTextViewText(R.id.tv_title_notification, title);
+        remoteViews.setTextViewText(R.id.tv_content_notification, content);
+        return remoteViews;
+    }
+}
